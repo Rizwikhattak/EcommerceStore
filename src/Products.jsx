ed80e3de2efc21a21e7c5ab7b55a54e177d91de3
Rizwikhattak <rizwikhattak4384@gmail.com>
2024-02-25 13:31:32 +0500

    Starting Product Page

--- src/Products.jsx
+++ src/Products.jsx
@@ -1,7 +1,26 @@
 import React from "react";
+import Filters from "./Components/Filters";
+import Sort from "./Components/Sort";
+import ProductList from "./Components/ProductList";
 
 const Products = () => {
-  return <div>Products</div>;
+  return (
+    <>
+      <section className="products">
+        <div className="filter-srction">
+          <Filters />
+        </div>
+        <div className="content-section">
+          <div className="sorts">
+            <Sort />
+          </div>
+          <div className="product-list">
+            <ProductList />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Products;
